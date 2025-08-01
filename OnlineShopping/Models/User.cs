70b5802bd2eaf0fef062e25afd45d5f59f5e9463
w-kiran <kirannn.js@gmail.com>
2025-08-01 06:26:56 +0545

    v2

---
+++ OnlineShopping/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace OnlineShopping.Models
+{ 
+    public class User
+    {
+        public int id { get; set;  }
+        [DataType(DataType.EmailAddress)]
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        [StringLength(50,MinimumLength =6,ErrorMessage ="Password invalid")]
+        public string Password { get; set; }
+        public string Mobile { get; set; }
+        public string Address { get; set; }
+        public string UserType { get; set; }
+    }
+}
--- OnlineShopping/obj/Debug/net8.0/OnlineShopping.AssemblyInfo.cs
+++ OnlineShopping/obj/Debug/net8.0/OnlineShopping.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineShopping")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10b85c86b0a04e3aef3f4cdc0064d2b0618ac9c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineShopping")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineShopping")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
